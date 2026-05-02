9a12c901322a8d0ffc8053517274a35dad8fb295
Dario Oddenino <branch13@gmail.com>
2026-05-02 15:20:23 +0200

    Sqitch status parser

--- src/main.zig
+++ src/main.zig
@@ -10,19 +10,16 @@
 // [] how to run a command in a folder
 // [] retrieve and parse all local git branches
 // [] get the content of the sqitch.plan for each
-// [] open the sqitch.plan file directly (or use a plan command, better)
-// [] parse sqitch plan content
 // [] match each branch to a migration
 // [] display the migrations with each branch
 // [] open the migration files on the side
 // [] select a migration to move there (both log and commit)
-// [] figure out how to find the current migration
 // [] BONUS: edit the migration files, maybe with the editor?
 
 const std = @import("std");
 const zz = @import("zigzag");
 const model = @import("./model.zig");
-const sqitch = @import("./sqitch.zig");
+const sqitch_parser = @import("./sqitch_parser.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -35,5 +32,5 @@ pub fn main() !void {
 }
 
 test {
-    _ = sqitch;
+    _ = sqitch_parser;
 }
--- src/sqitch_parser.zig
+++ src/sqitch_parser.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
 const mecha = @import("mecha");
 
-pub const Plan = struct { steps: []const PlanStep };
+pub const Plan = struct {
+    steps: []const PlanStep,
+};
 
+// TODO it'd be nice to have something different than strings
 pub const PlanStep = struct {
     deploy: []const u8,
     name: []const u8,
@@ -10,17 +13,30 @@ pub const PlanStep = struct {
     date: []const u8,
 };
 
-const string = mecha.ascii.range('a', 'Z').many(.{ .collect = false }).asStr();
+pub const Status = struct {
+    change: []const u8,
+    name: []const u8,
+    deployed: []const u8,
+    by: []const u8,
+};
 
-const colon = mecha.ascii.char(':').discard();
+const newLine = mecha.string("\n");
+
+const space = mecha.string(" ");
+
+const text =
+    mecha.many(mecha.ascii.not(mecha.string("\n")), .{ .collect = false });
+
+const line =
+    mecha.combine(.{
+        text,
+        newLine,
+    });
 
 const comment = mecha.combine(.{
     mecha.string("#"),
-    mecha.many(mecha.ascii.not(mecha.string("\n")), .{ .collect = false }),
-    mecha.string("\n"),
+    line,
 });
-
-const newLine = mecha.string("\n");
 
 const header = mecha.combine(.{
     mecha.many(comment, .{ .collect = false }),
@@ -30,17 +46,11 @@ const header = mecha.combine(.{
 fn fieldValue(label: []const u8) mecha.Parser([]const u8) {
     return mecha.combine(.{
         mecha.string(label).discard(),
-        mecha.many(mecha.string(" "), .{ .collect = false }).discard(),
+        mecha.many(space, .{ .collect = false }).discard(),
         mecha.many(mecha.ascii.not(mecha.string("\n")), .{ .collect = false }),
-        mecha.string("\n").discard(),
+        newLine.discard(),
     });
 }
-
-const description =
-    mecha.combine(.{
-        mecha.many(mecha.ascii.not(mecha.string("\n")), .{ .collect = false }),
-        mecha.string("\n"),
-    });
 
 const planStep = mecha.combine(.{
     fieldValue("Deploy"),
@@ -49,7 +59,7 @@ const planStep = mecha.combine(.{
     fieldValue("Date:"),
     mecha.many(newLine, .{ .collect = false }).discard(),
     // NOTE: for some reason this fails with `many`. I hope sqitch doesn't allow multiline descriptions...
-    description.discard(),
+    line.discard(),
     mecha.many(newLine, .{ .collect = false }).discard(),
 }).map(mecha.toStruct(PlanStep));
 
@@ -58,7 +68,17 @@ const plan = mecha.combine(.{
     mecha.many(planStep, .{ .collect = true }),
 }).map(mecha.toStruct(Plan));
 
-test {
+// NOTE: the status has a horrible format, I can only hope that the amount of rows is always the same.
+const status = mecha.combine(.{
+    line.discard(),
+    line.discard(),
+    fieldValue("# Change:"),
+    fieldValue("# Name:"),
+    fieldValue("# Deployed:"),
+    fieldValue("# By:"),
+}).map(mecha.toStruct(Status));
+
+test "plan" {
     const testing = std.testing;
     const allocator = std.heap.page_allocator;
 
@@ -92,4 +112,30 @@ test {
     try testing.expectEqualStrings("name0", actual.steps[0].name);
     try testing.expectEqualStrings("foo <foo@foo>", actual.steps[0].planner);
     try testing.expectEqualStrings("name1", actual.steps[1].name);
+}
+
+test "status" {
+    const testing = std.testing;
+    const allocator = std.heap.page_allocator;
+
+    // The new arena allocator takes an existing allocator and manages it.
+    // Note also this syntax, that's equivalent to `const arena = std.heap.ArenaAllocator.init(allocator);`
+    const arena: std.heap.ArenaAllocator = .init(allocator);
+    defer arena.deinit();
+
+    const content =
+        \\# On database sqitch
+        \\# Project:  sqitch
+        \\# Change:   66884058aaa662582f9795f90191c5b9909975b0
+        \\# Name:     bar
+        \\# Deployed: 2026-04-29 14:11:03 +0200
+        \\# By:       foo <foo@foo>
+        \\#
+        \\Nothing to deploy (up-to-date)
+    ;
+
+    const actual = (try status.parse(allocator, content)).value.ok;
+
+    try testing.expectEqualStrings("bar", actual.name);
+    try testing.expectEqualStrings("foo <foo@foo>", actual.by);
 }
